ea516948292d4661bd27a1af22c358790cabf688
haiderali309 <haiderali577772@gmail.com>
2026-03-14 21:08:48 +0500

    VRM issues

--- avatars/views.py
+++ avatars/views.py
@@ -18,6 +18,7 @@ from .serializers import (
 import logging
 import uuid
 import boto3
+from botocore.client import Config
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
@@ -51,6 +52,7 @@ class AvatarUploadUrlView(APIView):
             endpoint_url=settings.AWS_S3_ENDPOINT_URL,
             aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+            config=Config(s3={"addressing_style": "virtual"}),
         )
 
         try:
